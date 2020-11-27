436f3ca85052616b358018050f0fa47c53812e73
ahsansaif47 <ahsansaif047@gmail.com>
2020-11-27 23:47:48 +0500

    Update App.js
    Task 2

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import  React, { useState } from 'react';
+import { Text, View, StyleSheet, TextInput } from 'react-native';
+import Constants from 'expo-constants';
+
+// You can import from local files
+import AssetExample from './components/AssetExample';
+
+// or any pure javascript modules available in npm
 
 export default function App() {
+
+  const [price, setPrice] = useState("")
+  const [disc, setDisc] = useState("")
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Enter Price: </Text>
+      <TextInput style = {styles.input}
+        keyboard = 'numeric'
+        placeholder = "Price.."
+        onChangeText={price => setPrice(price)}
+      />
+      <Text>Enter Discount: </Text>
+      <TextInput style = {styles.input}
+        keyboard = 'numeric'
+        placeholder = "Discount.."
+        onChangeText={price => setPrice(price)}
+      />
+
+      <Text></Text>
     </View>
   );
 }
@@ -14,8 +35,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
   },
+  input: {
+    borderColor: "black",
+    borderWidth: 1,
+    width: 150,
+  }
 });
