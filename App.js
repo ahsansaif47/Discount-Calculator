f16c0d93e69dd548a007cbfa4f8f5e3f44caa4b2
ahsansaif47 <ahsansaif047@gmail.com>
2020-11-28 00:06:07 +0500

    Update App.js
    Task 3

--- App.js
+++ App.js
@@ -1,33 +1,41 @@
 import  React, { useState } from 'react';
-import { Text, View, StyleSheet, TextInput } from 'react-native';
+import { Text, View, StyleSheet, TextInput, Button } from 'react-native';
 import Constants from 'expo-constants';
-
-// You can import from local files
 import AssetExample from './components/AssetExample';
 
-// or any pure javascript modules available in npm
 
 export default function App() {
 
   const [price, setPrice] = useState("")
   const [disc, setDisc] = useState("")
+  const [fPrice, setfPrice] = useState("")
+  const [youSave, setSave] = useState("")
+
+  const priceAfter_discount = (price, discount) => {
+    setfPrice((price) - (price * (discount / 100)))
+    setSave((price * (discount / 100)))
+  }
 
   return (
     <View style={styles.container}>
       <Text>Enter Price: </Text>
       <TextInput style = {styles.input}
         keyboard = 'numeric'
-        placeholder = "Price.."
+        placeholder = "Enter Some Price.."
         onChangeText={price => setPrice(price)}
       />
       <Text>Enter Discount: </Text>
       <TextInput style = {styles.input}
         keyboard = 'numeric'
-        placeholder = "Discount.."
-        onChangeText={price => setPrice(price)}
+        placeholder = "Enter Some Discount.."
+        onChangeText={disc => setDisc(disc)}
       />
 
-      <Text></Text>
+      <Button title = "Calculate" 
+        onPress = {() => priceAfter_discount(price, disc)}> 
+      </Button>
+      <Text>You Saved: {youSave}</Text>
+      <Text>Final Price: {fPrice}</Text>
     </View>
   );
 }
